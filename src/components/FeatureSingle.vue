d92f0199a60f4867dc3c7cd2e70164ecdb65f300
Sherzod Axadov <sherzodaxadov69@gmail.com>
2023-08-01 22:11:46 +0500

    fix: feature section

---
+++ src/components/FeatureSingle.vue
@@ -0,0 +1,13 @@
+<template >
+    <div class="parent">
+        <img src="../assets/images/icon.svg" alt="logo">
+        <div class="next">
+            <h1>{{ feature.title }}</h1>
+            <p>{{ feature.subtitle }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps(['feature'])
+</script>
--- src/components/nextChatting.vue
+++ src/components/nextChatting.vue
@@ -1,72 +1,61 @@
 <template>
     <div class="container">
-       <div class="otasi">
-        <h1 class="nex_1">Features for a better experience</h1>
-         <div class="flex">
-            <div class="parent">
-             <img src="../assets/images/icon.svg" alt="logo">
-             <div class="next">
-                 <h1>Video mesaging</h1>
-                 <p>This software is very easy for you to <br>
-                  manage You can use it as you wish
-                </p>
-             </div>
-        </div>
+        <div class="otasi">
+            <h1 class="nex_1">Features for a better experience</h1>
 
-        <div class="parent">
-             <img src="../assets/images/22.svg" alt="logo">
-             <div class="next">
-                 <h1>Save your time</h1>
-                 <p>This software is very easy for you to <br>
-                  manage You can use it as you wish
-                </p>
-             </div>
+            <div class="flex">
+                <FeatureSingle v-for="(item, index) in features" :key="index" :feature="item" />
+            </div>
         </div>
-
-        <div class="parent">
-             <img src="../assets/images/33.svg" alt="logo">
-             <div class="next">
-                 <h1>Keep safe & private</h1>
-                 <p>This software is very easy for you to <br>
-                  manage You can use it as you wish
-                </p>
-             </div>
-        </div>
-         </div>
-       </div>
-     </div>
+    </div>
 </template>
-<script>
-export default {
-    
-}
+<script setup>
+import { ref } from "vue";
+import FeatureSingle from "./FeatureSingle.vue";
+
+const features = ref([
+    {
+        image: 'icon.svg', title: 'Video mesaging', subtitle: `This software is very easy for you to
+manage You can use it as you wish ` },
+    {
+        image: '22.svg', title: 'Save your time', subtitle: `This software is very easy for you to
+manage You can use it as you wish ` },
+    {
+        image: '33.svg', title: 'Keep safe & private', subtitle: `This software is very easy for you to
+manage You can use it as you wish ` }
+])
 </script>
 <style scoped>
-    .nex_1{
-        text-align: center;
-        font-size: 50px;
-    }
-    img{
-        border-radius: 50%;
-        height: 70px;
-        width: 70px;
-    }
-    .parent{
-        display: flex;
-        justify-content: space-around;
-    }
-    .parent h1{
-        font-size: 30px;
-    }
-    .otasi{
-        margin-top: 50px;
-    }
-    .otasi .parent{
-        margin-top: 30px;
-    }
-    .flex{
-        display: flex;
-        gap: 400px;
-        
-    }
+.nex_1 {
+    text-align: center;
+    font-size: 50px;
+}
+
+img {
+    border-radius: 50%;
+    height: 70px;
+    width: 70px;
+}
+
+.parent {
+    display: flex;
+    justify-content: space-around;
+}
+
+.parent h1 {
+    font-size: 30px;
+}
+
+.otasi {
+    margin-top: 50px;
+}
+
+.otasi .parent {
+    margin-top: 30px;
+}
+
+.flex {
+    display: flex;
+    justify-content: space-between;
+}
 </style>
